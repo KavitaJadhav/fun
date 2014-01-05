f6e5a859808a9197505e99669779c18c2be93c78
unknown <kavitama@Inkavitama.corporate.thoughtworks.com>
2014-01-05 12:58:24 +0530

    getting no of words using string

--- WC/WC.java
+++ WC/WC.java
@@ -6,11 +6,19 @@ public class WC{
 	public int getNoOfLines(String content) {
 	 	return content.split("\n").length;
     }
+	public int getNoOfWords(String content) {
+		contentArray = content.split("\n");
+		for(int i =0 ;i<contentArray.length ; i++)
+			newContent = newContent + contentArray[i]+" ";
+		return newContent.split(" ").length;
+    }
 	public static void main(String args[]){
 		WC wc = new WC();
 		String content = "good morning.\nhave a nice day";
 		int lines,words,characters;
 
 		System.out.println("lines "+wc.getNoOfLines(content));
+		System.out.println("words "+wc.getNoOfWords(content));
+
 	}
 }
